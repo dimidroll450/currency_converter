96c9cc745b73a0900ed76b49675981933534c3a7
kvashchauskas <dimakvasik@gmail.com>
2023-05-27 21:02:46 +0300

    currForm - MVP

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, DoCheck, Input, OnInit } from '@angular/core';
 import { GetCurrencyService } from './../services/get-currency.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -7,21 +8,51 @@ import { GetCurrencyService } from './../services/get-currency.service';
   styleUrls: ['./form.component.scss'],
   providers: [GetCurrencyService]
 })
-export class FormComponent implements DoCheck{
+export class FormComponent {
+  error!: string;
+
   @Input() list: any;
-  @Input() incomingValue!: number;
-  @Input() curInput!: number;
-  @Input() curOutput!: number;
 
-  ngDoCheck(): void {}
+  curForm: FormGroup = this._createForm();
 
-  convertValue(): number|undefined {
-    if (!this.incomingValue) {
+  constructor() {
+  }
+
+  private _createForm() {
+    return new FormGroup({
+      "valInput": new FormControl("", Validators.pattern("[0-9]")),
+      "selectInput": new FormControl("UAH"),
+      "valOutput": new FormControl({ value: "", disabled: true }),
+      "selectOutput": new FormControl(''),
+    });
+  }
+
+  changeOutputCur (e: any) {
+    this.curForm?.patchValue( { selectOutput: e.target.value });
+  }
+
+  convertValue () {
+    this.error = '';
+    let result = '';
+
+    const inputVal = this.curForm.get('valInput')?.value;
+    const inputCur = this.curForm.get('selectInput')?.value;
+    const outputCur = this.curForm.get('selectOutput')?.value;
+
+    if (!outputCur || !inputVal) {
       return;
     }
 
-    let result: number = this.incomingValue;
+    if (inputCur === 'UAH') {
+      console.log(outputCur);
+      const [currItem, ...rest] = this.list.filter((item: { cc: string; }) => item.cc === outputCur);
 
-    return result;
+      result = Math.abs(Number(inputVal) * Number(currItem.rate)).toFixed(2);
+    }
+
+
+    this.curForm.patchValue({
+      valOutput: result
+    });
   }
 }

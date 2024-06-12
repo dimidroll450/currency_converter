97c672828b721c9d13f9d99be6d46acd09cd7035
kvashchauskas <dimakvasik@gmail.com>
2024-06-12 09:43:08 +0300

    Update dependecies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ReactiveFormsModule }   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,21 +10,14 @@ import { FormComponent } from './form/form.component';
 import { CustomTextPipe } from './pipes/custom-text.pipe';
 import { CurListComponent } from './cur-list/cur-list.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    MainHeaderComponent,
-    FormComponent,
-    CustomTextPipe,
-    CurListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    ReactiveFormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        MainHeaderComponent,
+        FormComponent,
+        CustomTextPipe,
+        CurListComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }

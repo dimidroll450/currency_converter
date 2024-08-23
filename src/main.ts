6c0e7b5fa703df6e0e557988f5406d5b0ef80ad1
kvashchauskas <dimakvasik@gmail.com>
2024-08-23 17:16:34 +0300

    Add vercel analitic to project

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
+import { APP_INITIALIZER, isDevMode } from "@angular/core";
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { inject } from "@vercel/analytics";
 
 import { AppModule } from './app/app.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => {
+        inject({ mode: isDevMode() ? 'development' : 'production' })
+      }
+    }
+  ]
+})
   .catch(err => console.error(err));

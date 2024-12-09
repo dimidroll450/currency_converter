fbdc0dfae48f53bd895e1606311940aefea29914
kvashchauskas <dimakvasik@gmail.com>
2024-12-09 13:20:32 +0200

    Migrate to standalone components

--- src/app/cur-list/cur-list.component.ts
+++ src/app/cur-list/cur-list.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+
 import { CurrList } from '../utils/constants';
+import { CustomTextPipe } from '../pipes/custom-text.pipe';
 
 @Component({
     selector: 'cur-list',
     templateUrl: './cur-list.component.html',
     styleUrls: ['./cur-list.component.scss'],
-    standalone: false
+    imports: [UpperCasePipe, CustomTextPipe]
 })
 export class CurListComponent {
   @Input() list: CurrList = [];
